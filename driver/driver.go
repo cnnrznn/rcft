5a1caa3822956a07f3646762e6b86622705a70bc
Connor Zanin <cnnrznn@gmail.com>
2019-09-30 11:15:08 -0400

    create a function for running a consensus instance

--- driver/driver.go
+++ driver/driver.go
@@ -15,7 +15,7 @@ func hub(sendChans []chan rcft.Event,
     for {
         select {
         case e := <-recvChan:
-            //fmt.Println(e)
+            fmt.Println(e)
             sendChans[e.Pid] <- e
         default:
             //fmt.Println("Spinning")
@@ -23,11 +23,7 @@ func hub(sendChans []chan rcft.Event,
     }
 }
 
-func main() {
-    args := os.Args[1:]
-    n, _ := strconv.Atoi(args[0])
-    f, _ := strconv.Atoi(args[1])
-
+func instance(n, f, log_position int) {
     replicas := []rcft.Replica{}
     toreps := []chan rcft.Event{}
     fromreps := make(chan rcft.Event, 1024)
@@ -52,5 +48,15 @@ func main() {
     }
 
     go hub(toreps, fromreps)
+}
+
+func main() {
+    args := os.Args[1:]
+    n, _ := strconv.Atoi(args[0])
+    f, _ := strconv.Atoi(args[1])
+
+    for l:=0; ; l++ {
+        instance(n, f, l)
+    }
 }
 
