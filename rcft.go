69053db21da90b78fd9b8ca9676889846809654f
Connor Zanin <cnnrznn@gmail.com>
2019-10-24 19:05:30 -0400

    working on rcft with the new remote api

--- rcft.go
+++ rcft.go
@@ -1,118 +1,137 @@
 package rcft
 
 import (
-    "fmt"
-    "github.com/cnnrznn/dsdriver"
-    "github.com/cnnrznn/util"
-    "sync"
+	"encoding/json"
+	"fmt"
+	"github.com/cnnrznn/dsdriver"
+	"github.com/cnnrznn/util"
+	"sync"
 )
 
 type Replica struct {
-    value int
-    cardinality int
-    phaseno int
-    witness_count [2]int
-    message_count [2]int
+	value         int
+	cardinality   int
+	phaseno       int
+	witness_count [2]int
+	message_count [2]int
 }
 
 type Message struct {
-    value int
-    cardinality int
-    phaseno int
+	value       int
+	cardinality int
+	phaseno     int
 }
 
 type Event struct {
-    Pid int
-    m Message
+	Pid int     `json:"pid"`
+	m   Message `json:msg"`
+}
+
+func EventToBytes(e Event) (bytes []byte) {
+	bytes, err := json.Marshal(e)
+	if err != nil {
+		panic("Error encoding event")
+	}
+	return
+}
+
+func EventFromBytes(bytes []byte) (event Event) {
+	err := json.Unmarshal(bytes, &event)
+	if err != nil {
+		panic("Error decoding event")
+	}
+	return
 }
 
 func (e Event) Dest() int {
-    return e.Pid
+	return e.Pid
 }
 
 func (e Event) String() string {
-    return fmt.Sprintf("%v, %v", e.Pid, e.m)
+	return fmt.Sprintf("%v, %v", e.Pid, e.m)
 }
 
 func (r Replica) String() string {
-    return fmt.Sprintf("v:%v, c:%v, p:%v, ws:%v, ms:%v",
-                            r.value,
-                            r.cardinality,
-                            r.phaseno,
-                            r.witness_count,
-                            r.message_count)
+	return fmt.Sprintf("v:%v, c:%v, p:%v, ws:%v, ms:%v",
+		r.value,
+		r.cardinality,
+		r.phaseno,
+		r.witness_count,
+		r.message_count)
 }
 
 func NewReplica(value int) Replica {
-    return Replica { value : value,
-                     cardinality : 1,
-                     phaseno : 0,
-                     witness_count : [2]int{},
-                     message_count : [2]int{} }
+	return Replica{value: value,
+		cardinality:   1,
+		phaseno:       0,
+		witness_count: [2]int{},
+		message_count: [2]int{}}
 }
 
-func (r Replica) Consensus(n, f int, sendChan, recvChan chan dsdriver.Dester, wg *sync.WaitGroup) {
-    defer wg.Done()
+func (r Replica) Consensus(n, f int, sendChan, recvChan chan dsdriver.Message, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-    decision := 0
+	decision := 0
 
-    for r.witness_count[0] <= f && r.witness_count[1] <= f {
-        r.message_count = [2]int{}
-        r.witness_count = [2]int{}
+	for r.witness_count[0] <= f && r.witness_count[1] <= f {
+		r.message_count = [2]int{}
+		r.witness_count = [2]int{}
 
-        sendm := Message { value : r.value,
-                           cardinality : r.cardinality,
-                           phaseno : r.phaseno }
+		sendm := Message{value: r.value,
+			cardinality: r.cardinality,
+			phaseno:     r.phaseno}
 
-        for i := 0; i < n; i++ {
-            sendChan <- Event {i, sendm}
-        }
+		for i := 0; i < n; i++ {
+			//sendChan <- Event {i, sendm}
+			sendChan <- dsdriver.Message{Dest: i,
+				Bytes: EventToBytes(Event{i, sendm})}
+		}
 
-        for util.Sum(r.message_count[:]) < n - f {
-            d := <-recvChan
-            e := d.(Event)
-            msg := e.m
-            if msg.phaseno == r.phaseno {
-                r.message_count[msg.value]++
-                if msg.cardinality > n/2 {
-                    r.witness_count[msg.value]++
-                }
-            } else if msg.phaseno > r.phaseno {
-                sendChan <- e
-            }
-        }
+		for util.Sum(r.message_count[:]) < n-f {
+			d := <-recvChan
+			//e := d.(Event)
+			e := EventFromBytes(d.Bytes)
+			msg := e.m
+			if msg.phaseno == r.phaseno {
+				r.message_count[msg.value]++
+				if msg.cardinality > n/2 {
+					r.witness_count[msg.value]++
+				}
+			} else if msg.phaseno > r.phaseno {
+				sendChan <- d
+			}
+		}
 
-        if r.witness_count[0] > 0 {
-            r.value = 0
-        } else if r.witness_count[1] > 0 {
-            r.value = 1
-        } else if r.message_count[0] > r.message_count[1] {
-            r.value = 0
-        } else {
-            r.value = 1
-        }
+		if r.witness_count[0] > 0 {
+			r.value = 0
+		} else if r.witness_count[1] > 0 {
+			r.value = 1
+		} else if r.message_count[0] > r.message_count[1] {
+			r.value = 0
+		} else {
+			r.value = 1
+		}
 
-        r.cardinality = r.message_count[r.value]
-        r.phaseno = r.phaseno + 1
-    }
+		r.cardinality = r.message_count[r.value]
+		r.phaseno = r.phaseno + 1
+	}
 
-    if r.witness_count[0] > f {
-        decision = 0
-    } else {
-        decision = 1
-    }
+	if r.witness_count[0] > f {
+		decision = 0
+	} else {
+		decision = 1
+	}
 
-    for i := 0; i < n; i++ {
-        sendChan <- Event { i,
-                      Message { value : r.value,
-                                cardinality : n - f,
-                                phaseno : r.phaseno } }
-        sendChan <- Event { i,
-                      Message { value : r.value,
-                                cardinality : n - f,
-                                phaseno : r.phaseno + 1 } }
-    }
+	for i := 0; i < n; i++ {
+		sendChan <- dsdriver.Message{Dest: i,
+			Bytes: EventToBytes(Event{i, Message{value: r.value,
+				cardinality: n - f,
+				phaseno:     r.phaseno}})}
+		sendChan <- dsdriver.Message{Dest: i, Bytes: EventToBytes(Event{i,
+			Message{value: r.value,
+				cardinality: n - f,
+				phaseno:     r.phaseno + 1}})}
+	}
 
-    fmt.Println("Decided: ", decision)
+	fmt.Println("Decided: ", decision)
 }
-
--- rcft_test.go
+++ rcft_test.go
@@ -1,37 +1,65 @@
 package rcft
 
 import (
-    "fmt"
-    "github.com/cnnrznn/dsdriver"
-    "math/rand"
-    "sync"
-    "testing"
-    "time"
+	"fmt"
+	"github.com/cnnrznn/dsdriver"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
 )
 
 func TestReplicaInit(t *testing.T) {
-    r := NewReplica(0)
-    fmt.Println(r)
+	r := NewReplica(0)
+	fmt.Println(r)
 }
 
-func TestReplicaConsensus(t *testing.T) {
-    n := 101
-    f := 50
-    replicas := make([]Replica, n)
+//func TestReplicaConsensus(t *testing.T) {
+//    n := 101
+//    f := 50
+//    replicas := make([]Replica, n)
+//
+//    rand.Seed(time.Now().UnixNano())
+//
+//    for i := range replicas {
+//        replicas[i] = NewReplica(rand.Intn(2))
+//    }
+//
+//    fr, tos := dsdriver.Local(n, dsdriver.ReorderHub)
+//
+//    var wg sync.WaitGroup
+//    wg.Add(n)
+//    defer wg.Wait()
+//
+//    for i, r := range replicas {
+//        go r.Consensus(n, f, fr, tos[i], &wg)
+//    }
+//}
 
-    rand.Seed(time.Now().UnixNano())
+func runNode(replica Replica, n, f, i int, wg *sync.WaitGroup, nodes []string) {
+	fr, to := dsdriver.Remote(i, nodes)
+	replica.Consensus(n, f, fr, to, wg)
+}
 
-    for i := range replicas {
-        replicas[i] = NewReplica(rand.Intn(2))
-    }
+func TestReplicaRemote(t *testing.T) {
+	n := 5
+	f := 2
+	replicas := make([]Replica, n)
 
-    fr, tos := dsdriver.Local(n, dsdriver.ReorderHub)
+	var wg sync.WaitGroup
+	wg.Add(n)
+	defer wg.Wait()
 
-    var wg sync.WaitGroup
-    wg.Add(n)
-    defer wg.Wait()
+	rand.Seed(time.Now().UnixNano())
 
-    for i, r := range replicas {
-        go r.Consensus(n, f, fr, tos[i], &wg)
-    }
+	nodes := []string{"127.0.0.1:3330",
+		"127.0.0.1:3331",
+		"127.0.0.1:3332",
+		"127.0.0.1:3333",
+		"127.0.0.1:3334"}
+
+	for i := range replicas {
+		replicas[i] = NewReplica(rand.Intn(2))
+		go runNode(replicas[i], n, f, i, &wg, nodes)
+	}
 }
